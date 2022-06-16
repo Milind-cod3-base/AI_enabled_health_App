83e43f050546b6c3a2b3419b41fe03cda7b6b4a2
milind <milindchaudhary80@gmail.com>
2022-06-16 18:24:43 +0200

    Push notification module built using Plyer library's notification module

---
+++ push_notification.py
@@ -0,0 +1,16 @@
+
+from plyer import notification  
+
+import time
+
+# import TaskManager
+
+def PushNotif():
+    
+    notification.notify(title = "Alert", message= "Taskmanager.attribute")
+    
+    
+    
+
+
+PushNotif
