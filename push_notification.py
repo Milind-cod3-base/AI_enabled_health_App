03596d8e911b787cb507dfc7b2a698cb2082c05c
milind <milindchaudhary80@gmail.com>
2022-06-17 18:02:09 +0200

    pushNotif function is given a given parameter

--- push_notification.py
+++ push_notification.py
@@ -5,16 +5,14 @@ import time
 
 # import TaskManager
 
-def pushNotif():
+def pushNotif(x):     # x will be the message from Task Manager
     
-    notification.notify(title = "Alert", message= "Taskmanager.attribute")
+    notification.notify(title = "Alert", message= x)
     
 
 def pauseNotif():
-    pass
+    pass     #Do I really need pause notification function? 
 
-
-pushNotif()
 
     
     
