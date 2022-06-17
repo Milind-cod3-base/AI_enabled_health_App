d51d53588e021264ccf4a8c1fbb41ebd1e653206
milind <milindchaudhary80@gmail.com>
2022-06-17 02:28:37 +0200

    decrypt function built

--- encryptDatabase.py
+++ encryptDatabase.py
@@ -31,3 +31,21 @@ def encrypt(filename, key):
     # overwriting the new encrypted data into the original file
     with open(filename, "wb") as file:
         file.write(encrypted_data)
+
+
+# function to decrypt the databse
+def decrypt(filename, key):
+
+    f= Fernet(key)
+
+    with open(filename,"rb") as file:
+
+        # read encrypted data
+        encrypted_data = file.read()
+
+    # decrypt data
+    decrypted_data = f.decrypt(encrypted_data) 
+
+    # overwrite the original databse
+    with open(filename,"wb") as file:
+        file.write(decrypted_data)
