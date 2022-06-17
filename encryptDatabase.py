6a8f401ab61d280a906860bcdbe5ece57c6209e4
milind <milindchaudhary80@gmail.com>
2022-06-17 02:24:23 +0200

    function to encrypt the database built

--- encryptDatabase.py
+++ encryptDatabase.py
@@ -1,4 +1,3 @@
-import imp
 from cryptography.fernet import Fernet
 
 # function to write key into .key format file
@@ -14,4 +13,21 @@ def loadKey():
 
     # reading file using rb mode or read binary mode
     return open("key.key","rb").read()  
+
+
+# function to encrypt the data of a file using key
+def encrypt(filename, key):
+    f = Fernet(key)
+
+    # to open a file
+    with open(filename, "rb") as file:
+
+        # to read a file content and store it
+        file_data = file.read() 
     
+    # encrypting data
+    encrypted_data = f.encrypt(file_data)
+
+    # overwriting the new encrypted data into the original file
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
