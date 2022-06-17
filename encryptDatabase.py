13520931d787f5a1123d1e1703301bc768b7fd91
milind <milindchaudhary80@gmail.com>
2022-06-17 02:17:53 +0200

    laodkey function built to load the encryption key from .key file

--- encryptDatabase.py
+++ encryptDatabase.py
@@ -8,3 +8,10 @@ def writeKey():
     # wb stands for write binary mode
     with open("key.key","wb") as key_file:
         key_file.write(key)
+
+# function to loadkey from the .key file
+def loadKey():
+
+    # reading file using rb mode or read binary mode
+    return open("key.key","rb").read()  
+    
