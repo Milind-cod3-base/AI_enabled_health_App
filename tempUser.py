18899c9781f3ebff8311f902949edb9fa2792b39
milind <milindchaudhary80@gmail.com>
2022-06-18 14:42:11 +0200

    readName function added which can read the name of the temporary user logged in in to the app

--- tempUser.py
+++ tempUser.py
@@ -4,7 +4,15 @@
 and store it into the text file which could over written everytime a user logins into the app."""
 
 
+# function to store the temporary name into the text file tempUser.txt
 def storeName(name):
-    with open("tempUser.txt","w") as file:
+    with open("tempUsername.txt","w") as file:
         file.write(name)
 
+
+# function to read the name of temporary user from the file tempUser.txt
+def readName():
+    with open("tempUsername.txt","r") as file:
+        return file.read()
+
+print(readName())
