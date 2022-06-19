e6b6447c564b957f233369a72ee0bd56e6752cc5
milind <milindchaudhary80@gmail.com>
2022-06-19 12:04:26 +0200

    workflow direction added

--- push_notification.py
+++ push_notification.py
@@ -5,7 +5,11 @@ import time
 
 # import TaskManager
 
-def pushNotif(x):     # x will be the message from Task Manager
+""" x will be the message from the user interface (UI),
+ No need of connecting this from the database, 
+ it will directly read from the label which has been selected by the user """
+
+def pushNotif(x):     
     
     notification.notify(title = "Alert", message= x)
     
