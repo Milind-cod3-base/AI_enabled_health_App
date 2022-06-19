ddf1597ab33ba3e90d888ab59a9c0a0a5d924027
milind <milindchaudhary80@gmail.com>
2022-06-19 12:48:16 +0200

    Repeat notification function built which will notify the user in specify amount of time.

--- push_notification.py
+++ push_notification.py
@@ -9,14 +9,39 @@ import time
  No need of connecting this from the database, 
  it will directly read from the label which has been selected by the user """
 
-def pushNotif(x):     
+def pushNotif(task):     
     
-    notification.notify(title = "Alert", message= x)
+    notification.notify(title = "Alert", message= task)
     
+# pause function removed as it might not be important as I can just
 
-def pauseNotif():
-    pass     #Do I really need pause notification function? 
+# def pauseNotif():
+#     pass     #Do I really need pause notification function?
 
 
+
+# I need a snooze function or repeat notification function which will be sending notification in x number of hours
+def repeatNotif(hours,task):
     
+    # as sleep function takes input in seconds, converting hours into seconds
+    seconds = hours * 60 * 60
+   
+    # this will snooze the notification for required seconds
+    time.sleep(seconds)
+    
+    # after snooze notification again.
+    pushNotif(task)
+
+""" One issue might occur: can other functions of app operate properly, while Notification is in sleep mode?"""
+
+
+
+# these functions are dummy functions which can be useful for getting the value from the ui and storing it.
+# either it can store it into an empty txt file or simple call. lets see.
+#def getTask(task):
+    #return task
+
+#def getTime(hours):
+    #return hours
+
     
