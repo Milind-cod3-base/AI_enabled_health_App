d8e690c5cfdc5664383cfc17d984b837e771f13a
milind <milindchaudhary80@gmail.com>
2022-06-19 14:43:09 +0200

    Function to intake sensor data for ai and return the classified output built

---
+++ aiModel.py
@@ -0,0 +1,11 @@
+# this module contains the pretrained ai, this module needs to import the permissions for accelerometer and gyro from the phone
+# also it needs to import permissons such as tesnorflow etc. 
+
+
+# this function contains the ai, and takes input such from accelerometer and gyro
+def gruResponse(acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z):
+    
+    #This will contain the output response of the gru after calculation, like sitting standing walking
+    output = "sitting"
+
+    return output
