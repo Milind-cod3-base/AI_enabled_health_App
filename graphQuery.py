61c28a165c6ccd92ed0ee874238b1f15cc4b9e03
milind <milindchaudhary80@gmail.com>
2022-06-20 00:23:16 +0200

    comments added to functions and variables

--- graphQuery.py
+++ graphQuery.py
@@ -10,18 +10,30 @@ from datetime import timedelta
 # function to scan and display  daily activity
 def daily():
     
-    # ending of time 
+    # ending of time query
     upper_limit = datetime.datetime.now()
 
-    # starting of time
+    # starting of time query
     lower_limit = datetime.datetime.now() - timedelta(days= -1)
 
 
+
+# function to scan and display  weekly activity
 def weekly():
+
+    # ending of time query
     upper_limit = datetime.datetime.now()
+
+    # starting of time query
     lower_limit = datetime.datetime.now() - timedelta(days= -7)
 
 
+
+# function to scan and display  monthly activity
 def monthly():
+
+    # ending of time query
     upper_limit = datetime.datetime.now()
+
+    # starting of time query
     lower_limit = datetime.datetime.now() - timedelta(days = -30)
