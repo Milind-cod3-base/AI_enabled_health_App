a16b7ce93a1c5a4db0a42b9c6fc28b2f1d3eda1e
milind <milindchaudhary80@gmail.com>
2022-06-19 15:05:14 +0200

    databaseManager's function called to get the time and ai output and store it into tthe new database

--- aiModel.py
+++ aiModel.py
@@ -1,6 +1,6 @@
 # this module contains the pretrained ai, this module needs to import the permissions for accelerometer and gyro from the phone
 # also it needs to import permissons such as tesnorflow etc. 
-
+import databaseManager
 
 # this function contains the ai, and takes input such from accelerometer and gyro
 def gruResponse( time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z ):
@@ -8,5 +8,8 @@ def gruResponse( time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z ):
     #This will contain the output response of the gru after calculation, like sitting standing walking
     output = "sitting"
 
+    databaseManager.storeProfile(time, output)
 
-    return output
+
+    # below code might not be needed, could be revived later when needed
+    #return output    
