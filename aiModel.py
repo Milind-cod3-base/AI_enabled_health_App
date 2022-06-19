916485a66123bfd4aa3a880ad801b88334ef163a
milind <milindchaudhary80@gmail.com>
2022-06-19 20:02:01 +0200

    feedAi function built which feeds the sensors data from the device along with time and passes it into the ai model

--- aiModel.py
+++ aiModel.py
@@ -1,6 +1,9 @@
 # this module contains the pretrained ai, this module needs to import the permissions for accelerometer and gyro from the phone
 # also it needs to import permissons such as tesnorflow etc. 
 import databaseManager
+from plyer import accelerometer
+from plyer import gyroscope
+import datetime
 
 # this function contains the ai, and takes input such from accelerometer and gyro
 def gruResponse( time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z ):
@@ -13,3 +16,23 @@ def gruResponse( time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z ):
 
     # below code might not be needed, could be revived later when needed
     #return output    
+
+
+
+
+
+def feedAI():
+    accelerometer.enable()
+    gyroscope.enable()
+
+    while True:
+
+        time = datetime.datetime.now()
+
+        # gets accelerometers data in tuple format in 3 axes (x,y,z)
+        acc_x, acc_y, acc_z = accelerometer.acceleration
+        
+        # gets gyrometers data in tuple format in 3 axes 
+        gyr_x, gyr_y, gyr_z = gyroscope.rotation
+        
+        aiModel.gruResponse(time, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z)
