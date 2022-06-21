bbc7932138f79cb4a43ab341c4de98e053302b67
milind <milindchaudhary80@gmail.com>
2022-06-21 17:17:36 +0200

    gyro permission allow and deny done

--- permissionSensors.py
+++ permissionSensors.py
@@ -1,19 +1,25 @@
 # module responsible for granting sensors and memory access to the app for android
 from plyer.facades.accelerometer import Accelerometer
+from plyer.facades.gyroscope import Gyroscope
 
 # function to grant access for accelerometer 
 def allowAccelero():
 
-    Accelerometer().enable()
+    Accelerometer.enable()
 
 def denyAccelero():
 
     Accelerometer.disable()
 
 # function to grant access for gyrometer
-def gyroAccess():
-    pass
+def allowGyro():
+    
+    Gyroscope.enable()
 
+    
+# function to deny gyro access
+def denyGyro():
+    Gyroscope.disable()
 
 # function to grant access for memory storage 
 def memoryAccess():
