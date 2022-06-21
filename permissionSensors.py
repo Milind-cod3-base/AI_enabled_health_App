3fdd044bc018d6e38b345c706bb633cb15e32461
milind <milindchaudhary80@gmail.com>
2022-06-21 14:09:34 +0200

    dummy functions for accer, gyro, memory, bluetooth access built

---
+++ permissionSensors.py
@@ -0,0 +1,24 @@
+# module responsible for granting sensors and memory access to the app for android
+
+from kivy.utils import platform
+
+# function to grant access for accelerometer 
+def acceleroAccess():
+    pass
+
+
+# function to grant access for gyrometer
+def gyroAccess():
+    pass
+
+
+# function to grant access for memory storage 
+def memoryAccess():
+    pass
+
+# function to grant access for bluetooth
+def bluetooth():
+    pass
+
+    
+   
