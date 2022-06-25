146fe59f1f03a8647cfde0ee0c374cb8faadea6e
milind <milindchaudhary80@gmail.com>
2022-06-25 11:31:46 +0200

    deny gyroscope function built

--- permissionSensors.py
+++ permissionSensors.py
@@ -2,6 +2,7 @@
 from plyer.facades.accelerometer import Accelerometer
 from plyer.facades.gyroscope import Gyroscope
 
+
 # function to grant access for accelerometer 
 def allowAccelero():
 
@@ -16,7 +17,7 @@ def allowGyro():
     
     Gyroscope.enable()
 
-    
+
 # function to deny gyro access
 def denyGyro():
     Gyroscope.disable()
