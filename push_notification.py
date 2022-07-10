9162c452e26219cbedf91b63b22059c1ba0e5de9
milind <milindchaudhary80@gmail.com>
2022-07-10 14:18:21 +0200

    comments removed which wweere dummy

--- push_notification.py
+++ push_notification.py
@@ -8,22 +8,6 @@ import tempUser
 from timeloop import Timeloop
 from datetime import timedelta
 
-# import TaskManager
-
-""" x will be the message from the user interface (UI),
- No need of connecting this from the database, 
- it will directly read from the label which has been selected by the user """
-
-# def pushNotif(task):     
-    
-#     notification.notify(title = "Alert", message= task)
-    
-# pause function removed as it might not be important as I can just
-
-# def pauseNotif():
-#     pass     #Do I really need pause notification function?
-#task = ""
-
 
 # reading the timer in hours. Picks 2 as default
 notifTimer = int(tempUser.readNotificationTimer())
@@ -39,7 +23,7 @@ def repeatNotif():
     # reading the temporary stored task
     get_task = tempUser.readMotivation()
 
-    #print(get_task)
+    
     
 
     notification.notify(title="Alert", message=get_task)
