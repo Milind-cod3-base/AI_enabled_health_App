23023b641b887eb0926f6731b77ad3f4a9b3460d
milind <milindchaudhary80@gmail.com>
2022-07-10 14:31:39 +0200

    module description added

--- tempUser.py
+++ tempUser.py
@@ -1,7 +1,12 @@
 
 
-"""this contains a function to temporary take the user name 
-and store it into the text file which could over written everytime a user logins into the app."""
+"""
+    This module contains a function to temporarily take the user name,
+    notification timer setting, and current chosen motivation task for reminder
+    and store it into the different temporary text files which could over written everytime a 
+    new user logins into the app.
+
+"""
 
 
 # function to store the temporary name into the text file tempUser.txt
@@ -16,14 +21,15 @@ def readName():
         return file.read()
 
 
+
 """ It also stores the notification timer setting given by the user
 and also resets the screens at the time of logout to 2 hours so that it stays
 default"""
-
 # this will store the notification timer until logout
 def storeNotificationTimer(hours):
     with open("tempNotificationTimer.txt","w") as file:
         file.write(str(hours))
+
 
 # this function gets the selected timer value by the user
 def readNotificationTimer():
