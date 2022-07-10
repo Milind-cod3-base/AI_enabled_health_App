f260560988ab597ffa346283e65dab033ab7a54d
milind <milindchaudhary80@gmail.com>
2022-07-10 14:24:13 +0200

    comments added

--- push_notification.py
+++ push_notification.py
@@ -1,4 +1,5 @@
 
+
 from matplotlib.pyplot import title
 from plyer import notification  
 
@@ -9,13 +10,16 @@ from timeloop import Timeloop
 from datetime import timedelta
 
 
-# reading the timer in hours. Picks 2 as default
+# reading the timer in hours from the tempNotificationTimer.txt file. Picks 2 as default.
 notifTimer = int(tempUser.readNotificationTimer())
+
+# converting hours into seconds
 notifTimerSeconds = notifTimer * 60 * 60
 
 # instance given of Timeloop class
 tl = Timeloop()
 
+# attaching tl.job with the repeatNotif function
 @tl.job(interval= timedelta(seconds= notifTimerSeconds))
 # I need a snooze function or repeat notification function which will be sending notification in x number of hours
 def repeatNotif():
@@ -25,10 +29,11 @@ def repeatNotif():
 
     
     
-
+    # putting the already read task into the message of a notification banner
     notification.notify(title="Alert", message=get_task)
  
-
+# this function prevents to start the loop just after the app boot
+# instead, loop only starts when the remind me button is clicked in the UI
 def startTimer():
     tl.start(block=True)
 
