06d3adb4d9d2a9f13ae5c332905c91aa3824672b
milind <milindchaudhary80@gmail.com>
2022-08-18 11:37:15 +0200

    Refactor: autopep8 package used to set pep8 style

--- databaseManager.py
+++ databaseManager.py
@@ -25,131 +25,130 @@ import offlineAi
 from kivy.utils import platform
 
 # adding a new user via Create Account window
-def addUser(user,password):
+
+
+def addUser(user, password):
     # decrypting the database
-    key = encryptDatabase.loadKey() # loading the key 
-    encryptDatabase.decrypt("users.db",key)  # decrypting using key
-    
+    key = encryptDatabase.loadKey()  # loading the key
+    encryptDatabase.decrypt("users.db", key)  # decrypting using key
+
     # connecting to database
     conn = sqlite3.connect('users.db')
-    
+
     # create a cursor
     c = conn.cursor()
 
-    
     # add a record
-    c.execute("INSERT INTO data(username, password) VALUES (?,?)",(user,password))
+    c.execute("INSERT INTO data(username, password) VALUES (?,?)",
+              (user, password))
 
     conn.commit()
     conn.close()
 
-    
-    encryptDatabase.encrypt("users.db",key)  # encrypting using key
-    
+    encryptDatabase.encrypt("users.db", key)  # encrypting using key
 
 
 # creating a new table if it doesnt exist already
 def creatingTable():
-    
+
     # decrypting the database
-    key = encryptDatabase.loadKey() # loading the key 
-    encryptDatabase.decrypt("users.db",key)  # decrypting using key
+    key = encryptDatabase.loadKey()  # loading the key
+    encryptDatabase.decrypt("users.db", key)  # decrypting using key
 
     conn = sqlite3.connect('users.db')
-    
+
     c = conn.cursor()
 
     # Here just creating a table if it doesnt exist from before
-    c.execute("""CREATE TABLE if not exists data( 
-                username, password, name, age, weight, height, 
+    c.execute("""CREATE TABLE if not exists data(
+                username, password, name, age, weight, height,
                 job, gender )"""
-                )  
-    
+              )
 
     conn.commit()
     conn.close()
 
-
     # encrypting database after creating it and closing connection
 
-    encryptDatabase.encrypt("users.db",key)
+    encryptDatabase.encrypt("users.db", key)
 
 
-# checking and/or creating if tasks table doesnt exist in motivationTasks.db file
+# checking and/or creating if tasks table doesnt exist in
+# motivationTasks.db file
 def creatingTableTaskManager():
 
     conn = sqlite3.connect('motivationTasks.db')
-    
+
     c = conn.cursor()
 
     # Here just creating a table if it doesnt exist from before
     c.execute("""
                 CREATE TABLE if not exists tasks(
-                job, age, gender, movementProfile, 
+                job, age, gender, movementProfile,
                 task1, task2, task3, task4)
                 """
-                )  
-    
+              )
 
     conn.commit()
     conn.close()
 
 
-# for storing user profile into the users.db 
-def addProfile(name,age,weight,height,job,gender):
-    
+# for storing user profile into the users.db
+def addProfile(name, age, weight, height, job, gender):
+
     # decrypting the database
-    key = encryptDatabase.loadKey() # loading the key 
-    encryptDatabase.decrypt("users.db",key)  # decrypting using key
+    key = encryptDatabase.loadKey()  # loading the key
+    encryptDatabase.decrypt("users.db", key)  # decrypting using key
 
     conne = sqlite3.connect('users.db')
 
     cur = conne.cursor()
 
-    cur.execute("""UPDATE data 
+    cur.execute("""UPDATE data
                             SET name=?,
-                            age=?, 
-                            weight=?, 
+                            age=?,
+                            weight=?,
                             height=?,
                             job=?,
                             gender=?
-                            WHERE username=? """,(name,age,weight,height,job, gender, tempUser.readName()))    
-                            # validating user by the login username and only updating its details
-                            
+                            WHERE username=? """, (name, age, weight, height, job, gender, tempUser.readName()))
+    # validating user by the login username and only updating its details
+
     conne.commit()
     conne.close()
 
-    
-    encryptDatabase.encrypt("users.db",key)  # encrypting using key
+    encryptDatabase.encrypt("users.db", key)  # encrypting using key
 
 
 # this function takes in the username and gives out three output, job, age, gender which then could be used
-# for the task manager database query, along with one more parameter- movement profile (given by AI)
+# for the task manager database query, along with one more parameter-
+# movement profile (given by AI)
 def taskQuery(username):
-    
+
     # decrypting the database
-    key = encryptDatabase.loadKey() # loading the key 
-    encryptDatabase.decrypt("users.db",key)  # decrypting using key
+    key = encryptDatabase.loadKey()  # loading the key
+    encryptDatabase.decrypt("users.db", key)  # decrypting using key
 
     # a function to get the query for task manager, coloumns job, age, gender
     con = sqlite3.connect('users.db')
 
     cur = con.cursor()
     username = tempUser.readName()
-    
+
     # checks the username and gets the required job, age, gender
-    cur.execute("SELECT job, age, gender FROM data WHERE username=?", [username]) 
-    
+    cur.execute(
+        "SELECT job, age, gender FROM data WHERE username=?",
+        [username])
 
     item = cur.fetchall()
 
     con.commit()
     con.close()
-     
+
     # encrypting database after creating it and closing connection
 
-    encryptDatabase.encrypt("users.db",key)
-    
+    encryptDatabase.encrypt("users.db", key)
+
     # returns a list carry a tuple of data. For ex. [('ceo', '21-30', 'male')]
     return item
 
@@ -159,7 +158,7 @@ def createTableStoreProfile():
 
     # create and connect a database for storing movement profile
     conn = sqlite3.connect('movementProfile.db')
-    
+
     c = conn.cursor()
 
     # Here just creating a table if it doesnt exist from before
@@ -168,19 +167,19 @@ def createTableStoreProfile():
                 time, profile
                 )
                 """
-                )  
-    
+              )
 
     conn.commit()
     conn.close()
 
 
-# this will get the classified movemnt profile from the output and store it into the db file
+# this will get the classified movemnt profile from the output and store
+# it into the db file
 def storeProfile(time, profile):
 
     # create and connect a database for storing movement profile
     conn = sqlite3.connect('movementProfile.db')
-    
+
     c = conn.cursor()
 
     # Here just creating a table if it doesnt exist from before
@@ -188,52 +187,49 @@ def storeProfile(time, profile):
                 INSERT INTO data(time, profile) VALUES (?,?)
                 """, (time, profile)
 
-                )  
-    
+              )
 
     conn.commit()
-
 
     conn.commit()
     conn.close()
 
 
-
-# this function is responsible to take upper and lower limit of time 
+# this function is responsible to take upper and lower limit of time
 # and give an ordered output between the range which could be called in
 # graphQuery module
 def queryGraph(profile, lowerLimit, upperLimit):
-    
+
     conn = sqlite3.connect('movementProfile.db')
 
     c = conn.cursor()
 
     # getting time column between certain time_stamp range of a certain profile
-    c.execute("SELECT time FROM data WHERE profile=? AND time BETWEEN ? AND ?", 
-                (profile, lowerLimit, upperLimit))
+    c.execute("SELECT time FROM data WHERE profile=? AND time BETWEEN ? AND ?",
+              (profile, lowerLimit, upperLimit))
 
-
-    # items will give a list which contains timestamp and profile in the form of tuples
+    # items will give a list which contains timestamp and profile in the form
+    # of tuples
     items = c.fetchall()
-    
+
     c.commit()
     c.close()
 
-    # returning here because it will end the function and connection to db can close before this
+    # returning here because it will end the function and connection to db can
+    # close before this
     return items
 
 
-
-# a function to get the 4 tasks from the task manager based on the user's job, age, gender, profile
+# a function to get the 4 tasks from the task manager based on the user's
+# job, age, gender, profile
 def getTask():
 
-    # storing job, age and gender to the variables of the current user and its profile
+    # storing job, age and gender to the variables of the current user and its
+    # profile
     job, age, gender = taskQuery(tempUser.readName)[0]
-    
-   
+
     # putting this in a while loop so it keeps refreshing:
     while True:
-
 
         if platform == "android":
             # here a we need to constantly check the movement profile of user and given answer accordingly
@@ -244,11 +240,10 @@ def getTask():
             offlineAi.predict2()
 
             profile = offlineAi.predict3()
-            
+
         # dummy profile to run the app on Windows machine
         #profile = "sitting"
-        
-        
+
         # making a connection with task manager database and getting 4 tasks which will be
         # later displayed in the main screen in checkboxes
         conn = sqlite3.connect('motivationTasks.db')
@@ -257,26 +252,28 @@ def getTask():
         # returning four tasks based on the combination of job, age, gender, profile
         # motivationTasks.db has the tasks for every combination of user data
         cur.execute(""" SELECT task1, task2, task3, task4 FROM tasks
-                        WHERE job=? AND age=? 
-                        AND gender=? AND movementProfile=? """,(job, age, gender,profile))
+                        WHERE job=? AND age=?
+                        AND gender=? AND movementProfile=? """, (job, age, gender, profile))
 
         item = cur.fetchall()
 
         conn.commit()
         conn.close()
-        
-        # returning the 4 tasks in tuple format 
+
+        # returning the 4 tasks in tuple format
         return item[0]
 
-# CAUTION: clear table function, it will delete the mentioned table, use with caution
+# CAUTION: clear table function, it will delete the mentioned table, use
+# with caution
+
+
 def deleteTable(db):
 
     conn = sqlite3.connect(db)
-    
+
     # create a cursor
     c = conn.cursor()
 
-    
     # add a record
     c.execute("DROP TABLE data")
 
@@ -293,29 +290,28 @@ userPosition = 'empty'
 userGender = 'empty'
 
 # a function to display the userdata in the user data screen
+
+
 def displayData():
-    key = encryptDatabase.loadKey() # loading the key 
-    encryptDatabase.decrypt("users.db",key)  # decrypting using key
+    key = encryptDatabase.loadKey()  # loading the key
+    encryptDatabase.decrypt("users.db", key)  # decrypting using key
 
     conne = sqlite3.connect('users.db')
 
     cur = conne.cursor()
 
-    cur.execute("""  SELECT name, 
-                            age, 
-                            weight, 
+    cur.execute("""  SELECT name,
+                            age,
+                            weight,
                             height,
                             job,
                             gender
                             FROM data
-                            WHERE username=? """,[tempUser.readName()])    
-                            # validating user by the login username and only updating its details
-                            
-    
+                            WHERE username=? """, [tempUser.readName()])
+    # validating user by the login username and only updating its details
+
     items = cur.fetchall()
 
-    
-    
     global userName
     global userAge
     global userWeight
@@ -329,18 +325,12 @@ def displayData():
     userHeight = items[0][3]
     userPosition = items[0][4]
     userGender = items[0][5]
-    
-    
-    
+
     conne.commit()
     conne.close()
 
-    
-    encryptDatabase.encrypt("users.db",key)  # encrypting using key
-    
+    encryptDatabase.encrypt("users.db", key)  # encrypting using key
+
 
 # calling this function here to use the global variable
 displayData()
-
-
-
